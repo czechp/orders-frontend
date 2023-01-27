9d719606a89839f706cd7fd7180bdc19dacc9889
czechp <webcoderc@gmail.com>
2023-01-27 19:06:54 +0100

    Implement routing in the project

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
 import {Content, ContentWrapper, Layout, Navigation, TopBar} from "./layout/Layout";
 import TopBarCmp from "./layout/component/TopBarCmp";
 import NavigationCmp from "./layout/component/NavigationCmp";
-import PageCmp from "./component/PageCmp";
+import HomePage from "./page/HomePage/HomePage";
+import OrdersAll from "./page/OrdersAll/OrdersAll";
+import RegisterPage from "./page/Register/RegisterPage";
 
 function App() {
-  return (
-    <div className="App">
-
-        <Layout>
-            <TopBar>
-                <TopBarCmp />
-            </TopBar>
-            <ContentWrapper>
-                <Navigation>
-                    <NavigationCmp />
-                </Navigation>
-                <Content>
-                    <PageCmp title="Example page" />
-                </Content>
-            </ContentWrapper>
-        </Layout>
-    </div>
-  );
+    return (<div className="App">
+            <Layout>
+                <TopBar>
+                    <TopBarCmp/>
+                </TopBar>
+                <ContentWrapper>
+                    <BrowserRouter>
+                        <Navigation>
+                            <NavigationCmp/>
+                        </Navigation>
+                        <Content>
+                            <Routes>
+                                <Route path={"/"} element={<HomePage/>}/>
+                                <Route path={"/register"} element={<RegisterPage/>}/>
+                                <Route path={"/orders-all"} element={<OrdersAll/>}/>
+                            </Routes>
+                        </Content>
+                    </BrowserRouter>
+                </ContentWrapper>
+            </Layout>
+        </div>);
 }
 
 export default App;
