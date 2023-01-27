201bffeaa27200ba6728fa62d4198c4414c35d70
czechp <webcoderc@gmail.com>
2023-01-27 14:10:20 +0100

    Use loading spinner in page cmp

--- src/component/PageCmp.js
+++ src/component/PageCmp.js
@@ -1,9 +1,13 @@
 import styled from "styled-components";
+import React from "react";
+import LoadingSpinner from "./LoadingSpinner";
 
-const PageCmp = ({title}) => {
+const PageCmp = ({title, loaded = true}) => {
+
     return <Container>
         <Title>{title}</Title>
         <hr/>
+        {!loaded && <LoadingSpinnerWrapper><LoadingSpinner/></LoadingSpinnerWrapper>}
     </Container>
 }
 
@@ -13,6 +17,13 @@ const Container = styled.div`
 `;
 
 const Title = styled.h1`
-    text-align: left;
+  text-align: left;
+`;
+
+const LoadingSpinnerWrapper = styled.div`
+  width: 100%;
+  margin-top: 15rem;
+  display: flex;
+  justify-content: center;
 `;
 export default PageCmp;
