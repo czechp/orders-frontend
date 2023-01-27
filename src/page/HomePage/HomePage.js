6dbc7b320dbea9d208163d7d542842afbd60a668
czechp <webcoderc@gmail.com>
2023-01-27 19:07:09 +0100

    Few pages to test routing

---
+++ src/page/HomePage/HomePage.js
@@ -0,0 +1,9 @@
+import PageCmp from "../../component/PageCmp";
+
+const HomePage = ()=>{
+    return <PageCmp title="Home page">
+        Orders statistics here
+    </PageCmp>
+}
+
+export default HomePage;
