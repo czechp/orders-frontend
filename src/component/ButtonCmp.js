0bf7fbab1d651be033092ab57356ff19c091f5f1
czechp <webcoderc@gmail.com>
2023-01-28 18:38:47 +0100

    Button Cmp to unify design all buttons

---
+++ src/component/ButtonCmp.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+import colors from "../style/colors";
+
+const ButtonCmp = ({title, color = "transparent", onClick}) => {
+    function onClickInternal(event){
+        event.preventDefault();
+        onClick();
+    }
+    return <Button color={color} onClick={onClickInternal}>{title}</Button>;
+}
+
+const Button = styled.button`
+  width: 100%;
+  padding: 1rem;
+  border: 2px solid ${props => props.color};
+  color: ${props => props.color};
+  background-color: transparent;
+  border-radius: 1rem;
+  font-size: larger;
+
+  &:hover {
+    cursor: pointer;
+    background-color: ${props => props.color};
+    color: ${colors.main}
+  }
+`;
+export default ButtonCmp;
