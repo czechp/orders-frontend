9ee5afbf92f223fa778151702e75910f7e09731c
czechp <webcoderc@gmail.com>
2023-01-28 20:25:09 +0100

    Customise layout for statement component

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -5,11 +5,17 @@ export const Layout = styled.div`
   background-color: ${colors.background};
   color: ${colors.main};
   max-width:100%;
+  position: relative;
 `;
 
 export const TopBar = styled.div`
   height: 10vh;
   border-bottom: 2px solid;
+  width: 100vw;
+  max-width: 100%;
+  position: sticky;
+  top: 0;
+  z-index: 100;
 `;
 
 export const ContentWrapper = styled.div`
@@ -17,6 +23,7 @@ export const ContentWrapper = styled.div`
 `
 
 export const Content = styled.div`
+    position: relative;
     width: 80vw;
 `
 
