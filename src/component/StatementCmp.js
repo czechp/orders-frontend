328a36f91c7a6d3ac59f1085967c8c284173380d
czechp <webcoderc@gmail.com>
2023-01-28 20:27:17 +0100

    Statement componet

---
+++ src/component/StatementCmp.js
@@ -0,0 +1,52 @@
+import React from "react";
+import styled from "styled-components";
+import {StatementContext} from "../App";
+import colors from "../style/colors";
+
+const StatementCmp = () => {
+    const {statement, isError, showInfo} = React.useContext(StatementContext);
+    const [visibility, setVisibility] = React.useState(false);
+
+
+    React.useEffect(() => {
+        function hideWindow() {
+            setTimeout(() => {
+                setVisibility(false);
+                showInfo("");
+            }, 3000);
+        }
+
+        if (statement !== "") {
+            console.log("Something change")
+            setVisibility(true);
+            hideWindow();
+        }
+    }, [statement]);
+    return <Container error={isError}>
+        {visibility && <StatementContainer >
+            {statement}
+        </StatementContainer>}
+    </Container>
+}
+
+const Container = styled.div`
+  border-radius: 10px;
+  position: absolute;
+  top: 0;
+  right: calc(100vw / 2 - 100vw * 0.25);
+  width: 40vw;
+  height: 100%;
+
+`;
+const StatementContainer = styled.div`
+  font-size: x-large;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  color: ${props => props.error ? "red" : colors.basic };
+  border: 5px solid;
+  border-radius: 10px;
+  
+`;
+export default StatementCmp;
