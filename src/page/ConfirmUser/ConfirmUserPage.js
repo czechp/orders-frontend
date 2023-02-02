5dded7f718023b0bc3b9bc3fdcb0986f6faffade
czechp <webcoderc@gmail.com>
2023-02-02 14:36:54 +0100

    Remove unused imports

--- src/page/ConfirmUser/ConfirmUserPage.js
+++ src/page/ConfirmUser/ConfirmUserPage.js
@@ -10,7 +10,7 @@ import {useNavigate} from "react-router-dom";
 
 const ConfirmUserPage = () => {
     const axiosBackendHook = useAxios();
-    const {showInfo, statement} = React.useContext(StatementContext);
+    const {showInfo} = React.useContext(StatementContext);
     const navigate = useNavigate();
 
     const [confirmationToken, setConfirmationToken] = React.useState("");
