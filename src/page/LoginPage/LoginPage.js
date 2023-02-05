6e5fbc8bce2913df1dfe8b1850c0c6a43f65d8c8
czechp <webcoderc@gmail.com>
2023-02-05 19:33:56 +0100

    Form to user login

--- src/page/LoginPage/LoginPage.js
+++ src/page/LoginPage/LoginPage.js
@@ -1,7 +1,20 @@
-import PageCmp from "../../component/PageCmp";
+import React from "react";
 
-const LoginPage = ()=>{
-    return <PageCmp title="Login"></PageCmp>
+import PageCmp from "../../component/PageCmp";
+import FormCmp from "../../component/FormCmp";
+import InputTextCmp from "../../component/InputTextCmp";
+import ButtonCmp from "../../component/ButtonCmp";
+
+const LoginPage = () => {
+    const [login, setLogin] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    return <PageCmp title="Login">
+        <FormCmp>
+            <InputTextCmp title="Login" placeholder="Wpisz login" value={login} setValue={setLogin} />
+            <InputTextCmp title="Hasło" placeholder="Wpisz hasło" value={password} setValue={setPassword} />
+            <ButtonCmp title="Login" />
+        </FormCmp>
+    </PageCmp>
 }
 
 export default LoginPage;
