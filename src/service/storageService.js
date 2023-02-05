381f19385e37364f896a3cc1472d2e5d14305259
czechp <webcoderc@gmail.com>
2023-02-05 21:15:24 +0100

    Method to read value from storage

--- src/service/storageService.js
+++ src/service/storageService.js
@@ -1,6 +1,9 @@
 const storageService = {
     save: (label, data) => {
         localStorage.setItem(label, data);
+    },
+    read(label) {
+        return localStorage.getItem(label);
     }
 }
 
