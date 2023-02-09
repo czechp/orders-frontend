be67a2a3495cdcf29ec31dab2c5df11f403727b9
czechp <webcoderc@gmail.com>
2023-02-09 21:11:20 +0100

    Create new axios instance when you need make request

--- src/service/useAxiosService.js
+++ src/service/useAxiosService.js
@@ -2,13 +2,24 @@ import React, {useState} from "react";
 import {StatementContext} from "../App";
 import axios from "axios";
 import {BACKEND_URL} from "../constant/URL";
+import {getAuthorizationToken} from "./useAuthenticationService";
 
-const axiosBackend = axios.create({baseURL: BACKEND_URL});
 
 const httpErrorHandler = (error, showErrorInfo) => {
     console.log(error)
     const message = error.response.data.message || "Nieznany błąd";
     showErrorInfo(message, true);
+}
+
+function createBackendInstance() {
+    const axiosBackendInstance = axios.create({baseURL: BACKEND_URL});
+    axiosBackendInstance.interceptors.request.use((request) => {
+       const authorizationToken = getAuthorizationToken();
+       if(authorizationToken)
+           request.headers["Authorization"]=`Basic ${authorizationToken}`
+        return request;
+    });
+    return axiosBackendInstance;
 }
 
 const useAxiosService = () => {
@@ -20,12 +31,17 @@ const useAxiosService = () => {
 
     const [axiosBackendHook] = useState({
         post: ((endpoint, body, successHandler, errorHandler = handleError) => {
-            axiosBackend.post(endpoint, body)
+            createBackendInstance().post(endpoint, body)
+                .then(successHandler)
+                .catch(errorHandler)
+        }),
+        get: ((endpoint, successHandler, errorHandler = handleError) => {
+            createBackendInstance().get(endpoint)
                 .then(successHandler)
                 .catch(errorHandler)
         }),
         configureAuthorizationHeader: (authorizationHeader) => {
-            axiosBackend.interceptors.request.use((config) => {
+            createBackendInstance().interceptors.request.use((config) => {
                 if (authorizationHeader)
                     config.headers.Authorization = `Basic ${authorizationHeader}`;
                 else
