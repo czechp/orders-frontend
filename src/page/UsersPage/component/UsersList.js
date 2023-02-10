dc8906d99c8fdc75513617d41a0933fdeeea25da
czechp <webcoderc@gmail.com>
2023-02-10 12:29:54 +0100

    Create list of users

--- src/page/UsersPage/component/UsersList.js
+++ src/page/UsersPage/component/UsersList.js
@@ -2,17 +2,49 @@ import React from "react";
 import styled from "styled-components";
 import useAxiosService from "../../../service/useAxiosService";
 import LoadingWrapper from "../../../component/LoadingWrapper";
+import dateFormatter from "../../../service/dateFormatter";
+import {Table, Td, Th, Thead, Tr} from "../../../style/table";
 
 const UsersList = () => {
     const axiosService = useAxiosService();
     const [users, setUsers] = React.useState(null);
 
-    React.useEffect(() => {axiosService.get("/api/users", (response) => setUsers(response.data))}, [])
+    React.useEffect(() => {
+        axiosService.get("/api/users", (response) => setUsers(response.data))
+    }, [])
 
 
     return <LoadingWrapper loaded={users}>
-        <Container></Container>
+        <Container>
+            {users && <Table>
+                <Thead>
+                    <Th>Id:</Th>
+                    <Th>Login:</Th>
+                    <Th>Email:</Th>
+                    <Th>Rola:</Th>
+                    <Th>Potwierdzenie adresu email:</Th>
+                    <Th>Data utowrzenia:</Th>
+                </Thead>
+                <tbody>
+                {
+                    users.map((user, id) => <UserRow key={`${user.id}-${Math.random()}`} user={user}/>)
+                }
+                </tbody>
+            </Table>}
+        </Container>
     </LoadingWrapper>
+}
+
+const UserRow = ({user}) => {
+    //TODO: add admin activation
+    return <Tr>
+        <Td>{user.id}</Td>
+        <Td>{user.username}</Td>
+        <Td>{user.email}</Td>
+        <Td>{user.userRole}</Td>
+        <Td>{user.confirmed ? "TAK" : "NIE"}</Td>
+        <Td>{dateFormatter.toFormattedDate(user.createdAt)}</Td>
+    </Tr>
 }
 
 const Container = styled.div`
