814d8465e4e76cdb61d54756e6bbc4c7d2fba86a
czechp <webcoderc@gmail.com>
2023-02-12 16:16:02 +0100

    Remove debug log

--- src/service/useAxiosService.js
+++ src/service/useAxiosService.js
@@ -6,7 +6,6 @@ import {getAuthorizationToken} from "./useAuthenticationService";
 
 
 const httpErrorHandler = (error, showErrorInfo) => {
-    console.log(error)
     const message = error.response.data.message || "Nieznany błąd";
     showErrorInfo(message, true);
 }
