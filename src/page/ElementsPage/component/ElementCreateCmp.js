a670d47400927aa8e088f2cec0b9cadf6b1894f3
czechp <webcoderc@gmail.com>
2023-03-02 11:16:13 +0100

    ElementCreateCmp - send post request in order to create a new element after click in create button

--- src/page/ElementsPage/component/ElementCreateCmp.js
+++ src/page/ElementsPage/component/ElementCreateCmp.js
@@ -9,8 +9,10 @@ import useElementCreateData from "./useElementCreateData";
 import SelectCmp from "../../../component/SelectCmp";
 import InputTextCmp from "../../../component/InputTextCmp";
 import colors from "../../../style/colors";
+import {StatementContext} from "../../../App";
 
-const ElementCreateCmp = () => {
+const ElementCreateCmp = ({reload}) => {
+    const {showInfo} = React.useContext(StatementContext);
     const axiosService = useAxiosService();
     const modalWindowHandler = useModalWindow();
     const createData = useElementCreateData();
@@ -24,18 +26,47 @@ const ElementCreateCmp = () => {
         });
     }
 
+    function elementCreated() {
+        showInfo("Element został stworzony");
+        modalWindowHandler.hideModalWindow();
+        reload();
+        createData.clear();
+    }
+
+    function createElementRequest() {
+        const requestBody = {
+            producerId: createData.producer.value,
+            categoryId: createData.category.value,
+            providerId: createData.provider.value,
+            name: createData.name.value,
+            description: createData.description.value,
+            url: createData.url.value,
+            serialNumber: createData.serialNumber.value
+        };
+
+        axiosService.post("/api/elements", requestBody, elementCreated);
+    }
+
+    function createButtonOnClick() {
+        if (createData.validateAllData())
+            createElementRequest();
+        else
+            showInfo("Sprwdź poprawność danych", true);
+    }
+
 
     return <FormCmp>
         <ButtonCmp title="Dodaj element" onClick={() => modalWindowHandler.showModalWindow()}/>
         <ModalWindowCmp title="Dodaj nowy element" modalHandler={modalWindowHandler}>
             <InputTextCmp value={createData.name.value} setValue={createData.name.setValue} title="Nazwa"
-                          placeholder="Wpisz nazwę elementu" validation={createData.name.validation} />
-            <InputTextCmp value={createData.serialNumber.value} setValue={createData.serialNumber.setValue} title="Numer seryjny"
-                          placeholder="Wpisz numer seryjny" validation={createData.serialNumber.validation} />
+                          placeholder="Wpisz nazwę elementu" validation={createData.name.validation}/>
+            <InputTextCmp value={createData.serialNumber.value} setValue={createData.serialNumber.setValue}
+                          title="Numer seryjny"
+                          placeholder="Wpisz numer seryjny" validation={createData.serialNumber.validation}/>
             <InputTextCmp value={createData.description.value} setValue={createData.description.setValue} title="Opis"
-                          placeholder="Wpisz opis" />
+                          placeholder="Wpisz opis"/>
             <InputTextCmp value={createData.url.value} setValue={createData.url.setValue} title="Link"
-                          placeholder="Wklej link" />
+                          placeholder="Wklej link" validation={createData.url.validation}/>
             <SelectCmp value={createData.producer.value} setValue={createData.producer.setProducer}
                        options={generateSelectArray(producers)}
                        title="Wybierz producenta"/>
@@ -45,7 +76,7 @@ const ElementCreateCmp = () => {
             <SelectCmp value={createData.provider.value} setValue={createData.provider.setProvider}
                        options={generateSelectArray(providers)}
                        title="Wybierz dostawce"/>
-            <ButtonCmp title="Dodaj element" color={colors.success} />
+            <ButtonCmp title="Dodaj element" color={colors.success} onClick={createButtonOnClick}/>
         </ModalWindowCmp>
     </FormCmp>
 }
