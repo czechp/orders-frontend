ba7eb973783b865032c22d2229c676fb67578504
czechp <webcoderc@gmail.com>
2023-03-02 11:15:42 +0100

    ElementsPage.js - state to reload components

--- src/page/ElementsPage/ElementsPage.js
+++ src/page/ElementsPage/ElementsPage.js
@@ -1,12 +1,18 @@
 import React from "react";
 import PageCmp from "../../component/PageCmp";
 import ElementListCmp from "./component/ElementListCmp";
-import ElementCreateCmp from "../ElementDetailsPage/component/ElementCreateCmp";
+import ElementCreateCmp from "./component/ElementCreateCmp";
 
 const ElementsPage = () => {
+    const [reload, setReload] = React.useState(false);
+
+    function reloadPage() {
+        setReload((prev) => !prev);
+    }
+
     return <PageCmp title={"Elementy"}>
-        <ElementCreateCmp/>
-        <ElementListCmp/>
+        <ElementCreateCmp reload={reloadPage}/>
+        <ElementListCmp reload={reload}/>
     </PageCmp>
 }
 
