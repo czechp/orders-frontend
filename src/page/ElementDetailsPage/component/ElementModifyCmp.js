62f1ef92217ba8ba9a17fb5214d84a3eb5a4915c
czechp <webcoderc@gmail.com>
2023-03-03 20:08:39 +0100

    Component to modify element info

--- src/page/ElementDetailsPage/component/ElementModifyCmp.js
+++ src/page/ElementDetailsPage/component/ElementModifyCmp.js
@@ -1,10 +1,19 @@
 import FormCmp from "../../../component/FormCmp";
 import ButtonCmp from "../../../component/ButtonCmp";
 import colors from "../../../style/colors";
+import useModalWindow from "../../../service/useModalWindow";
+import ModalWindowCmp from "../../../component/ModalWindowCmp";
+import ElementModifyInfoCmp from "./ElementModifyInfoCmp";
 
-const ElementModifyCmp = ({element}) => {
+const ElementModifyCmp = ({element, reload}) => {
+    const modalWindowHandler = useModalWindow();
     return <FormCmp>
-        <ButtonCmp title="Modyfikuj element" color={colors.warning}/>
+        <ButtonCmp title="Modyfikuj element" color={colors.warning} onClick={modalWindowHandler.showModalWindow}/>
+        <ModalWindowCmp modalHandler={modalWindowHandler}
+                        title={`Modyfikacja elementu: ${element.name}`}
+        >
+            <ElementModifyInfoCmp element={element} closeModal={modalWindowHandler.hideModalWindow} />
+        </ModalWindowCmp>
     </FormCmp>
 }
 
