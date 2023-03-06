fa356c7aab9a31463b73a85f369ffefa7eb33443
czechp <webcoderc@gmail.com>
2023-03-06 14:40:36 +0100

    Component to associated element

---
+++ src/page/ElementDetailsPage/component/AssociatedElementRemoveCmp.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ModalWindowCmp from "../../../component/ModalWindowCmp";
+import ButtonCmp from "../../../component/ButtonCmp";
+import useAxiosService from "../../../service/useAxiosService";
+import {StatementContext} from "../../../App";
+
+const AssociatedElementRemoveCmp = ({modalWindowHandler, element, elementParentId}) => {
+    const axiosService = useAxiosService();
+    const {showInfo} = React.useContext(StatementContext);
+
+    function elementRemoved() {
+        showInfo("Element został usunięty");
+        modalWindowHandler.hideModalWindow();
+    }
+
+    const removeAssociatedBtnOnClick = () => {
+        const params = {associatedElementId: element.id};
+        axiosService.delete(`/api/elements/associated-elements/${elementParentId}`, elementRemoved, params);
+    };
+
+    return element ? <ModalWindowCmp modalHandler={modalWindowHandler} title={`Usuń element - ${element.name}`}>
+        <ButtonCmp title="Usuń powiązany element" onClick={removeAssociatedBtnOnClick}/>
+    </ModalWindowCmp> : <></>
+}
+
+export default AssociatedElementRemoveCmp;
--- src/page/ElementDetailsPage/component/AssociatedElementsCmp.js
+++ src/page/ElementDetailsPage/component/AssociatedElementsCmp.js
@@ -1,11 +1,25 @@
-import styled from "styled-components";
-import useGetRequest from "../../../service/useGetRequest";
+import React from "react";
 import ElementListCmp from "../../ElementsPage/component/ElementListCmp";
+import styled from "styled-components";
+import useModalWindow from "../../../service/useModalWindow";
+import AssociatedElementRemoveCmp from "./AssociatedElementRemoveCmp";
 
 const AssociatedElementsCmp = ({element}) => {
-    return <ElementListCmp reload={()=>{}} url={`api/elements/associated-elements/${element.id}`} />
+    const [elementToRemove, setElementToRemove] = React.useState();
+    const modalWindowHandler = useModalWindow();
+    const rowOnClick = (element) => {
+        setElementToRemove(element);
+        modalWindowHandler.showModalWindow();
+    };
+
+    return <Container>
+        <ElementListCmp reload={() => {
+        }} url={`api/elements/associated-elements/${element.id}`} rowOnClick={rowOnClick}/>
+        <AssociatedElementRemoveCmp modalWindowHandler={modalWindowHandler} element={elementToRemove} elementParentId={element.id}/>
+    </Container>
 }
 
-const Container = styled.div``;
-
+const Container = styled.div`
+  width: 100%;
+`;
 export default AssociatedElementsCmp;
