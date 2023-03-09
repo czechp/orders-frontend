62c9bd93f162b914b24c01f9bab4a4db7d2daed6
czechp <webcoderc@gmail.com>
2023-03-09 11:00:13 +0100

    Resolve problem with React.useCallback dependencies

--- src/page/OrdersPage/component/OrdersList.js
+++ src/page/OrdersPage/component/OrdersList.js
@@ -4,41 +4,57 @@ import useAxiosService from "../../../service/useAxiosService";
 import {Table, Tbody, Td, Th, Thead, Tr} from "../../../style/table";
 import dateFormatter from "../../../service/dateFormatter";
 import useSortingParams from "../../../service/useSortingParams";
+import LoadingWrapper from "../../../component/LoadingWrapper";
 
 const OrdersList = ({state, title}) => {
-    const [orders, setOrders] = React.useState([]);
-    const [sortField, setSortField]=React.useState();
+    const [orders, setOrders] = React.useState();
+    const [sortParams, setSortParams] = React.useState(null);
     const axiosService = useAxiosService();
     const generateSortingParams = useSortingParams();
 
+
     const getOrdersRequest = React.useCallback(() => {
         let params = {};
-        if(sortField)
-            params = generateSortingParams(sortField);
+        if (sortParams)
+            params = sortParams;
 
-        axiosService.get("/api/orders", (response) => setOrders(response.data), { ...params,state});
-    }, [setOrders, axiosService, state, sortField]);
+        axiosService.get("/api/orders", (response) => setOrders(response.data), {...params, state});
+    }, [setOrders, axiosService, state, sortParams]);
 
+    const determineSortingParameters = (field) => {
+        setSortParams(generateSortingParams(field));
+    };
 
     React.useEffect(() => {
         getOrdersRequest()
     }, [getOrdersRequest]);
 
     return <Container>
-        <Title>{title}</Title>
-        <Table>
-            <Thead>
-                <Tr>
-                    <Th onClick={()=>{setSortField("id")}}>Id</Th>
-                    <Th onClick={()=>{setSortField("orderInfoName")}}>Nazwa</Th>
-                    <Th onClick={()=>{setSortField("createdBy")}}>Właściciel</Th>
-                    <Th onClick={()=>{setSortField("createdAt")}}>Data utowrzenia</Th>
-                </Tr>
-            </Thead>
-            <Tbody>
-                {orders.map((order) => <OrderRow key={`${order.id}-${Math.random()}`} order={order}/>)}
-            </Tbody>
-        </Table>
+        <LoadingWrapper loaded={orders}>
+            {orders && <>
+                <Title>{title}</Title>
+                <Table>
+                    <Thead>
+                        <Tr>
+                            <Th onClick={() => {
+                                determineSortingParameters("id")
+                            }}>Id</Th>
+                            <Th onClick={() => {
+                                determineSortingParameters("orderInfoName")
+                            }}>Nazwa</Th>
+                            <Th onClick={() => {
+                                determineSortingParameters("createdBy")
+                            }}>Właściciel</Th>
+                            <Th onClick={() => {
+                                determineSortingParameters("createdAt")
+                            }}>Data utowrzenia</Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        {orders.map((order) => <OrderRow key={`${order.id}-${Math.random()}`} order={order}/>)}
+                    </Tbody>
+                </Table></>}
+        </LoadingWrapper>
     </Container>
 }
 
