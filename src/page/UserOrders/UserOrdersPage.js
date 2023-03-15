9d4699b80ca0bbfa1e435c33b915831909ec2ab3
czechp <webcoderc@gmail.com>
2023-03-15 11:57:33 +0100

    Display list of orders for given user

--- src/page/UserOrders/UserOrdersPage.js
+++ src/page/UserOrders/UserOrdersPage.js
@@ -1,9 +1,18 @@
 import PageCmp from "../../component/PageCmp";
+import OrdersList from "../OrdersPage/component/OrdersList";
+import React from "react";
+import {useNavigate} from "react-router-dom";
 
-const UserOrdersPage = ()=>{
+const UserOrdersPage = () => {
+    const navigate = useNavigate();
+    function navigateToDetails(order) {
+        navigate("/order-details", {state: {id: order.id, name: order.name}});
+    }
     return <PageCmp title="Twoje zamowienia">
-
+        <OrdersList url={"/api/orders/user"} rowOnClick={navigateToDetails} reload={()=>{}} state="PREPARATION" title="W przygotowaniu"/>
+        <OrdersList url={"/api/orders/user"} rowOnClick={navigateToDetails} reload={()=>{}} state="EXECUTION" title="W realizacji"/>
+        <OrdersList url={"/api/orders/user"} rowOnClick={navigateToDetails} reload={()=>{}} state="CLOSED" title="Zamknięte"/>
     </PageCmp>
 }
 
-export  default UserOrdersPage;
+export default UserOrdersPage;
