4010bca397bda54613e39998cd4f4dd6f1b75b7d
czechp <webcoderc@gmail.com>
2023-03-21 14:54:37 +0100

    Display list of orders in execution in OrdersInExecutionPage.js

--- src/page/OrdersInExecutionPage/OrdersInExecutionPage.js
+++ src/page/OrdersInExecutionPage/OrdersInExecutionPage.js
@@ -1,8 +1,9 @@
 import PageCmp from "../../component/PageCmp";
+import OrdersList from "../OrdersPage/component/OrdersList";
 
 const OrdersInExecutionPage = () => {
     return <PageCmp title="Zamówienia do realizacji">
-
+        <OrdersList url="/api/orders" state="EXECUTION" reload={()=>{}}/>
     </PageCmp>
 }
 
