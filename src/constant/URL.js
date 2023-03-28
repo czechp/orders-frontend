24d9465247f0422992cafd2065b5493f1bd2d8c5
czechp <webcoderc@gmail.com>
2023-03-28 09:54:25 +0200

    Deplot

--- src/constant/URL.js
+++ src/constant/URL.js
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "http://localhost:8080";
-// export const BACKEND_URL = "http://192.168.6.100:9000";
+// export const BACKEND_URL = "http://localhost:8080";
+export const BACKEND_URL = "http://192.168.6.100:9000";
