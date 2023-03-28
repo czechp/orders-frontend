0dc90d31c94e020df5a2a5bc75431e2a0be1d94b
czechp <webcoderc@gmail.com>
2023-03-28 07:35:40 +0200

    Added loading wrapper to improve visibility

--- src/page/OrderedPositions/component/OrderedPositionsCmp.js
+++ src/page/OrderedPositions/component/OrderedPositionsCmp.js
@@ -3,28 +3,31 @@ import useGetRequest from "../../../service/useGetRequest";
 import {BACKEND_URL} from "../../../constant/URL";
 import {Table, Tbody, Td, Th, Thead, Tr} from "../../../style/table";
 import positionUnitTranslator from "../../../service/translator/positionUnitTranslator";
+import LoadingWrapper from "../../../component/LoadingWrapper";
 
 const OrderedPositionsCmp = () => {
     const {result: positions} = useGetRequest(`${BACKEND_URL}/api/orders/positions/ordered`);
     return <Container>
-        <Table>
-            <Thead>
-                <Tr>
-                    <Th>Id</Th>
-                    <Th>Nazwa</Th>
-                    <Th>Nr. seryjny</Th>
-                    <Th>Producent</Th>
-                    <Th>Ilość</Th>
-                    <Th>Jednostka</Th>
-                    <Th>Zamówienie</Th>
-                    <Th>Właściciel</Th>
-                    <Th>Nr.zamówienia</Th>
-                </Tr>
-            </Thead>
-            <Tbody>
-                {positions.map((position)=><OrderedPositionRow key={`${position.id}-${Math.random()}`} position={position} />)}
-            </Tbody>
-        </Table>
+        <LoadingWrapper loaded={positions}>
+            <Table>
+                <Thead>
+                    <Tr>
+                        <Th>Id</Th>
+                        <Th>Nazwa</Th>
+                        <Th>Nr. seryjny</Th>
+                        <Th>Producent</Th>
+                        <Th>Ilość</Th>
+                        <Th>Jednostka</Th>
+                        <Th>Zamówienie</Th>
+                        <Th>Właściciel</Th>
+                        <Th>Nr.zamówienia</Th>
+                    </Tr>
+                </Thead>
+                <Tbody>
+                    {positions.map((position)=><OrderedPositionRow key={`${position.id}-${Math.random()}`} position={position} />)}
+                </Tbody>
+            </Table>
+        </LoadingWrapper>
     </Container>
 };
 
